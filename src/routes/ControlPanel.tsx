8948d7d82c879243414bc301a31a220f32bf2661
CodeDoctorDE <proficodergames@gmail.com>
2020-07-01 16:06:47 +0200

    typescript

---
+++ src/routes/ControlPanel.tsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class ControlPanel extends Component {
+    static propTypes: {};
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+
+export default ControlPanel;
