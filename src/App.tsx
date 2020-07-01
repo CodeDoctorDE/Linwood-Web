b1cdb861b90605cbdd8580bd48c41aa38a0c74cd
CodeDoctorDE <proficodergames@gmail.com>
2020-07-01 18:28:44 +0200

    add default

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,64 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ControlPanel from './routes/ControlPanel';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/users">Users</Link>
+          </li>
+          <li>
+            <Link to="/cp">CP</Link>
+          </li>
+        </ul>
+      </nav>
+
+      {/* A <Switch> looks through its children <Route>s and
+          renders the first one that matches the current URL. */}
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/users">
+          <Users />
+        </Route>
+        <Route path="/cp">
+          <ControlPanel />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
     </div>
+  </Router>
   );
 }
 
--- src/routes/ControlPanel.tsx
+++ src/routes/ControlPanel.tsx
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import { Button, Link } from '@material-ui/core';
+
 
 class ControlPanel extends Component {
     static propTypes: {};
     render() {
         return (
             <div>
-                
+            <Button variant="contained" color="primary">
+              Hello World
+            </Button>
+                 <Link>Test
+                 </Link>
             </div>
         );
     }
